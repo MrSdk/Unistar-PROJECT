ea4a843a3179b21ec6cc9318a6dd78e65114f612
mrsdk <mrsdk1902@mail.ru>
2019-09-12 16:39:18 +0500

    test2

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
     // C4iTnlr2NYJQ94ei 
-mongoose.connect(`mongodb://saydakram:C4iTnlr2NYJQ94ei@cluster0-shard-00-00-nxhyj.mongodb.net:27017,cluster0-shard-00-01-nxhyj.mongodb.net:27017,cluster0-shard-00-02-nxhyj.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`, { useNewUrlParser: true }).then((res) => {
+mongoose.connect(`mongodb+srv://saydakram:C4iTnlr2NYJQ94ei@cluster0-nxhyj.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true }).then((res) => {
     console.log("Db connected !!!");
 
     // mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, { useNewUrlParser: true }).then((res) => {
