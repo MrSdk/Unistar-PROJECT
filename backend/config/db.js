dcbb8292b528ec68b9a8e4d07bdb601fe792fb41
mrsdk <mrsdk1902@mail.ru>
2019-09-12 16:42:11 +0500

    test3

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
     // C4iTnlr2NYJQ94ei 
-mongoose.connect(`mongodb+srv://saydakram:C4iTnlr2NYJQ94ei@cluster0-nxhyj.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true }).then((res) => {
+mongoose.connect(`mongodb+srv://saydakram:C4iTnlr2NYJQ94ei@cluster0-nxhyj.mongodb.net/test?retryWrites=true&w=majority`).then((res) => {
     console.log("Db connected !!!");
 
     // mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, { useNewUrlParser: true }).then((res) => {
